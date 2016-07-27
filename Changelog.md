a6420ad46da70ecd3bed176636db06d1641f5905
seigneurfuo <kitouagnesa@yahoo.fr>
2016-07-27 12:48:12 +0200

    Déplacment du code changelog dans un fichier à part

---
+++ Changelog.md
@@ -0,0 +1,54 @@
+# Changements
+
+	25/07/2016
+	Modification du module MAL: Il télécgarge désomais les covers des animés automatiquement
+	
+	24/07/2016
+	Lors du remplissage des informations par MyanimeList, la cover de l'animé est téléchargée
+	
+    23/07/2016
+	Ajout d'icones sur les boutons et les onglets,
+    Ajout d'un bouton qui permet d'afficher ses animés préférés d'un seul clic
+						   
+    20/07/2016
+	Correction du bug de texte dans l'entrée MAL,
+    Ajout d'une entrée Ajouté le dans les informations des animés
+						   
+    09/07/2016
+	Ajout d'une barre de recherche pour parcourir les animés,
+    Il est maintenant possible de revenir sur le jour en cours sur le planning grace a un bouton,
+    La barre de recherche peut etre vidée grace un bouton,
+    code: 426 lignes
+						   
+	07/07/2016
+	Désormais l'application demandera à sauvegarder les données uniquement si quelque chose a été modifié depuis l'ouverture
+	 
+	05/07/2016
+	Correction de bug: Les tranches d'heures sont numérotées a partir de 1 désormais,
+    Pour rajouter un animé dans l'onglet le journal, il suffit de double cliquer sur celui ci ou de cliquer sur insérer,
+	Nouvelle fonction: Il est désormais possible de remplire les informations grace a MyAnimelist
+				   
+	04/07/2016
+	Depuis l'ongelt planning, il suffit de cliquer dans la liste des animés a voir pour l'ajouter dans le journal,
+	Correction de bug: Si rien n'a été entré dans la journal, l'ajout d'un animé faisait apparaitre un saut de ligne en première position,
+	Il est maintenant impossible d'entrer du texte dans la champ ID
+				   
+	03/07/2016
+	Dans l'onglet planning, il est désormais possible de voir les animés en cours. Pratique pour savoir ou l'on en est
+	 
+	28/06/2016
+	Mise a jour de l'interface,
+	Ajout d'une fonction de calcul des animés
+				   
+	26/06/2016
+	Correction du bug qui empéchait l'enregistrement final (commit de la bdd),
+	Ajout d'une fonction planning qui permet d'afficher et de rajouter des animés vus grace a un calendrier.,
+	Ajout d'une table dans la bdd pour la fonction précédente,
+	Réorganisation légère de l'interface,
+	Ajout d'informations chargées a partir du code dans l'ongelt \A propos\
+				   
+	19/06/2016
+	Changement de l'interface: Passage a une interface a onglet pour acceuillir les futures fonctions
+	 
+	12/06/2016
+	Utilisation de pyQt car je ne voulais pas utiliser Tkinter]}
--- MyAnimeManagerGui.py
+++ MyAnimeManagerGui.py
@@ -43,7 +43,7 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
 log.addHandler(console_handler)
-#---------------------------------------------------------
+
 # Détails
 toDo = ["Utiliser le titre de l'animé plutot que son identifiant pour la complétion MAL",
         "Continuer la fonction de MAJ de la base de donnée pour les prochaines versions",
@@ -53,36 +53,6 @@ toDo = ["Utiliser le titre de l'animé plutot que son identifiant pour la complét
         "Coder la fenetre directement dans le code - sans utilisation de QtDesign",
         "Ou alors enregistrer le contenu du fichier d'interface dans une docstring dans le code",
         "Renommer les noms des élements génériques. Exemple: Bouton1, bouton2..."]
-
-changelog = {"Date":"Modifications",
-             "25/07/2016":["Modification du module MAL: Il télécgarge désomais les covers des animés automatiquement"],
-             "24/07/2016":["Lors du remplissage des informations par MyanimeList, la cover de l'animé est téléchargée"],
-             "23/07/2016":["Ajout d'icones sur les boutons et les onglets",
-                           "Ajout d'un bouton qui permet d'afficher ses animés préférés d'un seul clic"],
-             "20/07/2016":["Correction du bug de texte dans l'entrée MAL",
-                           "Ajout d'une entrée Ajouté le dans les informations des animés"],
-             "09/07/2016":["Ajout d'une barre de recherche pour parcourir les animés",
-                           "Il est maintenant possible de revenir sur le jour en cours sur le planning grace a un bouton",
-                           "La barre de recherche peut etre vidée grace un bouton",
-                           "code: 426 lignes"],
-             "07/07/2016":["Désormais l'application demandera à sauvegarder les données uniquement si quelque chose a été modifié depuis l'ouverture"],
-             "05/07/2016":["Correction de bug: Les tranches d'heures sont numérotées a partir de 1 désormais",
-                           "Pour rajouter un animé dans l'onglet le journal, il suffit de double cliquer sur celui ci ou de cliquer sur insérer",
-                           "Nouvelle fonction: Il est désormais possible de remplire les informations grace a MyAnimelist"],
-             "04/07/2016":["Depuis l'ongelt planning, il suffit de cliquer dans la liste des animés a voir pour l'ajouter dans le journal",
-                           "Correction de bug: Si rien n'a été entré dans la journal, l'ajout d'un animé faisait apparaitre un saut de ligne en première position",
-                           "Il est maintenant impossible d'entrer du texte dans la champ ID"],
-             "03/07/2016":["Dans l'onglet planning, il est désormais possible de voir les animés en cours. Pratique pour savoir ou l'on en est"],
-             "28/06/2016":["Mise a jour de l'interface",
-                           "Ajout d'une fonction de calcul des animés"],
-             "26/06/2016":["Correction du bug qui empéchait l'enregistrement final (commit de la bdd)",
-                           "Ajout d'une fonction planning qui permet d'afficher et de rajouter des animés vus grace a un calendrier.",
-                           "Ajout d'une table dans la bdd pour la fonction précédente",
-                           "Réorganisation légère de l'interface",
-                           "Ajout d'informations chargées a partir du code dans l'ongelt \"A propos\""],
-             "19/06/2016":["Changement de l'interface: Passage a une interface a onglet pour acceuillir les futures fonctions"],
-             "12/06/2016":["Utilisation de pyQt car je ne voulais pas utiliser Tkinter"]}
-
 
 # Fonctions générale a l'application
 def creation_de_la_bdd():
@@ -157,16 +127,6 @@ class Menu(PyQt4.QtGui.QMainWindow, PyQt4.uic.loadUiType("./data/gui.ui")[0]): #
         self.liste_rafraichir()
         self.planning_afficher()
         self.animes_vus_afficher()
-        self.a_propos()
-
-
-    # Fonction qui permet de remplir le texte de l'onglet "A propos"
-    def a_propos(self):
-        pp = pprint.PrettyPrinter(indent=2)
-        strChangelog = pprint.pformat(changelog)
-        strChangelog = strChangelog.translate(None, "\"'{}[]\\,").strip("Date: Modifications")
-        self.plainTextEdit.setPlainText(strChangelog)
-
 
     # La fonction qui efface les entrés (les instructions auraient pus etres contenues dans liste_affiche mais je souhaitais séparer les deux blocs)
     def liste_rafraichir(self, titreRecherche=False, favorisRecherche=False):
