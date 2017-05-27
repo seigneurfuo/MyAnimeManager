4f24c1007bc5cad8cceadaaff8d99b960af366d1
seigneurfuo <seigneurfuo@gmail.com>
2017-05-27 03:18:36 +0200

    version 0.24.13

--- ressources/core/info.py
+++ ressources/core/info.py
@@ -1,7 +1,7 @@
 # Informations sur l'application
 
 __titre__ = "MyAnimeManager"
-__version__ = "0.24.12"
+__version__ = "0.24.13"
 __auteur__ = "seigneurfuo"
 __db_version__ = 6
 __dateDeCreation__ = "12/06/2016"
